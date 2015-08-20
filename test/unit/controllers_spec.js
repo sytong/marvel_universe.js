fc0d6c380206d7266472dacb9d18c19b49bc5394
Willie Tong <tongsinyin@gmail.com>
2015-08-20 23:21:47 +0800

    modified unit test

--- test/unit/controllers_spec.js
+++ test/unit/controllers_spec.js
@@ -2,18 +2,19 @@
 
 /* jasmine specs for controllers */
 describe('MarvelController', function() {
-  var scope, ctrl, $httpBackend, mock_credentials;
+  var scope, ctrl, $httpBackend;
   
   beforeEach(module('marvelApp'));
+  beforeEach(module('marvelApp.apiServices'));
 
   beforeEach(inject(function(_$httpBackend_, $rootScope, $controller, _MyCredentials_) {
     scope = $rootScope.$new();
     scope.marvel = {};
 
-    mock_credentials = _MyCredentials_;
+    spyOn(_MyCredentials_, "generate").and.returnValue({a: 'b'});
 
     $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET("http://localhost:1234/test/?a=b&nameStartsWith=Hulk").
+    $httpBackend.expectGET("http://gateway.marvel.com/v1/public/characters?a=b&nameStartsWith=Hulk").
       respond(
         {'data': 
           {'results': [{'id': 1, 'name': 'Hulk', 'description': 'Bruce Banner'}]}
@@ -27,9 +28,7 @@ describe('MarvelController', function() {
   }));
 
   it('should get me the Hulk', function() {
-    spyOn(mock_credentials, "generate").and.returnValue("a=b");
     scope.marvel.searchTerm = 'Hulk';
-    scope.marvel.base_url = "http://localhost:1234/test/";
     scope.marvel.search();
     $httpBackend.flush();
     expect(scope.marvel.searchResults[0]).toBe('Hulk: Bruce Banner');
